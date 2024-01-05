12297ff8784bfbb5ff1a440836bc0f2e1b8e5b26
RebeccaSh <rebecca.r.shen@gmail.com>
2024-01-04 21:14:14 -0800

    Add files via upload

---
+++ personal portfolio/script.js
@@ -0,0 +1,24 @@
+function sendMail(){
+    let parms ={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+
+    }
+    emailjs.send("service_oln9wn9","template_p92qcxg",parms)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=> alert(err)
+    );
+
+    
+}
